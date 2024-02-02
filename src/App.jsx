4cf878abef653f22c50adb8e1accd464fae2be1a
Shepardy22 <inv72kmaster@gmail.com>
2024-02-02 10:03:49 -0300

    Criando o projeto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Pagamento PIX</h1>
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
